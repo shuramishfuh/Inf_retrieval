df2c54cc333cf9207e829f4b6c675b842ba8cac8
SHU RAMISH FUH <agamatimtim@gmail.com>
2021-02-22 18:08:01 +0200

    Update Inf_retrieval.py
    Implemented intersection using Binary and linear search

--- Inf_retrieval/Inf_retrieval.py
+++ Inf_retrieval/Inf_retrieval.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 def convertOrAddToindex(wordsAndfileName, index):
     for wf in wordsAndfileName:
-        print(Fore.YELLOW,"adding to index from ", wf[1])
+        print(Fore.YELLOW, "adding to index from ", wf[1])
         for word in wf[0]:
             if word in index:
                 w = index[word]
@@ -29,6 +29,42 @@ def changeFileNameToDocId(wordsAndfileName, docIds):
             id = JsonSer.getDocId(6)
             docIds[id] = str(file[1])
             file[1] = id
+
+
+def intersectUsingBinarySearch(postingOne, postingTwo):
+    common = []
+    if len(postingOne) < len(postingTwo):
+        for posting in postingOne:
+            index = bisect.bisect_left(postingTwo, posting)
+            if postingTwo[index] == posting:
+                common.append(posting)
+    else:
+        for posting in postingTwo:
+            index = bisect.bisect_left(postingOne, posting)
+            if postingOne[index] == posting:
+                common.append(posting)
+    if len(common) < 1:
+        print("no common words")
+        return -1
+    else:
+        return common
+
+
+def IntersectUsingLinearSearch(postingOne, postingTwo):
+    common = []
+    if len(postingOne < len(postingTwo)):
+        for posting in postingOne:
+            if str(posting) in postingTwo:
+                common.append(posting)
+    else:
+        for posting in postingTwo:
+            if str(posting) in postingOne:
+                common.append(posting)
+    if len(common) < 1:
+        print("no common words")
+        return -1
+    else:
+        return common
 
 
 print("started")
@@ -54,6 +90,7 @@ loadedIndex = JsonSer.readInvertedIndex("InvertedIndex.json")
 # print("size of index is 15% of all files")
 # print("***********************************************************************")
 print("***************************data 1 Ends******************************")
+
 
 # ***********************************************************************
 
